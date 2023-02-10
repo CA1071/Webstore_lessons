ed74cc89f753458b7fdd44729de89b62374b9dd1
Carlos Arro <119028008+CA1071@users.noreply.github.com>
2023-02-10 10:03:16 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+var cacheName = "lessonStore-v1";
+var cacheFiles = [
+    'index.html',
+    'product.js',
+    'style.css',
+    'webstore.webmanifest',
+    'images/art.png',
+    'images/biology.png',
+    'images/chemistry.png',
+    'images/cs.png',
+    'images/english.png',
+    'images/geography.png',
+    'images/history.png',
+    'images/lesson.png',
+    'images/math.png',
+    'images/physics.png',
+    'images/psychology.png'
+]
+
+
+self.addEventListener('install', (e) =>{
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache)=>{
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    )
+});
+
+self.addEventListener('fetch',function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(r){
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then(function(response){
+                return caches.open(cacheName).then(function(cache){
+                    cache.put(e.request,response.close());
+                    return response;
+                })
+            })
+        })
+    )
+});
